c4a8494cc5bfea9814da67d72fba4539cead7b88
catrofe <lord.rico10@gmail.com>
2022-10-12 08:54:21 -0300

    FEAT: Criando Entities

---
+++ src/main/java/com/login/login/entities/User.java
@@ -0,0 +1,25 @@
+package com.login.login.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String name;
+    private String cpf;
+    private String phone;
+    private String password;
+}
